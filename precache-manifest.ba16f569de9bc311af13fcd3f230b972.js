c21f486845723cad25ade13df17ead05c0ab3c4f
Jim <oberadin25@gmail.com>
2020-08-19 02:24:36 +0300

    Updates

---
+++ precache-manifest.ba16f569de9bc311af13fcd3f230b972.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258d3c821fb9a5a6b33ff1f585267de0",
+    "url": "/daddee/index.html"
+  },
+  {
+    "revision": "0417f8d30e8b2b9954cd",
+    "url": "/daddee/static/css/main.8e8e6161.chunk.css"
+  },
+  {
+    "revision": "16f02c72ada8658e952e",
+    "url": "/daddee/static/js/2.a6083f24.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/daddee/static/js/2.a6083f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0417f8d30e8b2b9954cd",
+    "url": "/daddee/static/js/main.f59a0ba3.chunk.js"
+  },
+  {
+    "revision": "5257df9ea8f7a1e79b0d",
+    "url": "/daddee/static/js/runtime-main.e1bbebc7.js"
+  },
+  {
+    "revision": "ccd93f39a4c5b8e90f43b64069a30f76",
+    "url": "/daddee/static/media/smirk.ccd93f39.png"
+  }
+]);
