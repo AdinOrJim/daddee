e04163176b54b03a83bf03448c343a2f6c11857a
Jim <oberadin25@gmail.com>
2020-08-19 03:00:31 +0300

    removed basename

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
     <div className="App">
       <Navbara />
       <div className="container mx-auto">
